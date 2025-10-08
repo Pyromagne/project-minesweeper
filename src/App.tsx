179f26257aeb1b884ab948c3098144f54e50f348
Pyromagne <growganoth826@gmail.com>
2025-10-08 23:16:51 +0800

    Create UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Board from './game/components/Board';
 import type { t_cell } from './game/minesweeper';
 import useGameEngine from './hooks/useGameEngine';
@@ -9,11 +9,13 @@ import { difficultiesName } from './game/difficulties';
 import { useStorage } from './hooks/useStorage';
 import timestampToString from './game/utils/timestampToString';
 import timestampToMilliseconds from './game/utils/timestampToMilliseconds';
+import { FaPen } from 'react-icons/fa6';
 
 const App = () => {
   const { board, setBoard, gameOver, setGameOver, gameWon, setGameWon, gameBoard, bombCount, setDifficulty } = useGameEngine();
   const { hours, minutes, seconds, milliseconds, start, pause, reset, time, isPause } = useStopwatch();
   const { name, setName, scores, addScore } = useStorage();
+  const [editName, setEditName] = useState(false);
 
   useEffect(() => {
     resetGame();
@@ -122,75 +124,94 @@ const App = () => {
   };
 
   return (
-    <div className="flex min-h-svh azeret-mono background-default" onContextMenu={(e) => e.preventDefault()}>
-      <aside className='w-1/5 h-svh flex flex-col items-center py-2 pl-2'>
-        <h1 className="text-xl font-bold azeret-mono">
-          <span className='text-amber-400 bg-black px-2'>PROJECT</span>
-          <span className='text-red-400 bg-white pl-2'>[mine]</span><span className='text-green-400 bg-white pr-2'>sweeper</span>
-        </h1>
-        <div>
-          {difficultiesName.map((diff, index) => {
-            return <button key={index} onClick={() => setDifficulty(diff)} className={`m-1 p-2 rounded text-sm ${gameBoard.name === diff ? 'bg-amber-400' : 'hover:bg-amber-500 cursor-pointer'}`}>{diff}</button>
-          })}
+    <div className="azeret-mono flex flex-col h-svh select-none" onContextMenu={(e) => e.preventDefault()}>
+      <nav className='background-default flex items-center justify-between shadow-lg h-12 px-3 z-10'>
+        <div className="w-fit text-xl font-bold azeret-mono">
+          <span className='text-amber-400'>PROJECT</span>
+          <span className='text-red-400'>[mine]</span><span className='text-green-400'>sweeper</span>
         </div>
 
-        <p className='mt-auto azeret-mono w-full'>Created by <span className='text-red-400 font-medium'>Pyromagne</span></p>
-      </aside>
-
-      <main className='w-3/5 h-svh relative flex flex-col items-center'>
-        <p className='azeret-mono text-center mt-4 text-2xl'>{`${pad(hours)}:${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`}</p>
-
-        <div className='mt-6 drop-shadow-2xl'>
-          <Board board={board} onLeftClick={handleLeftClick} onRightClick={handleRightClick} />
+        <div className='flex items-center gap-2'>
+          <span className='font-medium text-xl py-[.15rem] bg-amber-400 rounded-lg px-2'>{gameBoard.name}</span>
+          <span className='bg-gray-200 rounded-lg shadow-inner px-1 azeret-mono text-center text-2xl'>{`${pad(hours)}:${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`}</span>
         </div>
+      </nav>
 
-        <div className='absolute bottom-0 right-0 mb-2 mr-2'>
-          <input type="text"
-            className='w-fit outline-none text-right'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-      </main>
+      <div className='flex grow'>
+        <aside className='w-1/5 flex flex-col bg-gray-700/5'>
+          <div className='flex'>
+            {difficultiesName.map((diff, index) => {
+              return <button key={index} onClick={() => setDifficulty(diff)} className={`grow m-1 p-2 rounded text-sm ${gameBoard.name === diff ? 'bg-amber-400' : 'hover:bg-amber-500 cursor-pointer'}`}>{diff}</button>
+            })}
+          </div>
 
-      <aside className='w-1/5 h-svh py-2 flex flex-col'>
-        <p className='bg-amber-300 font-medium px-2'>Global Scoreboard</p>
-        <ul className="text-sm max-h-72 overflow-y-auto">
+          <div className='mt-auto p-2'>
+            {gameOver && <p className="azeret-mono text-red-400 font-semibold text-4xl tracking-wider w-full text-center">GAME OVER</p>}
+            {gameWon && <p className="azeret-mono text-green-400 font-semibold text-4xl tracking-wider w-full text-center">YOU WIN</p>}
+            <button
+              className={(gameOver || isPause ? 'bg-gray-400 cursor-not-allowed' : 'bg-amber-400 hover:bg-amber-500 cursor-pointer') + ` px-4 py-2  rounded w-full`}
+              onClick={pause}
+            >
+              Pause
+            </button>
+            <button
+              className="mt-2 px-4 py-2 bg-amber-400 rounded  w-full hover:bg-amber-500 cursor-pointer"
+              onClick={resetGame}
+            >
+              {gameOver ? 'Retry' : 'Restart'}
+            </button>
+          </div>
+        </aside>
 
-        </ul>
-        <p className='bg-amber-300 font-medium px-2'>Local ScoreBoard</p>
-        <ul className="text-sm max-h-72 overflow-y-auto pl-2">
-          {scores
-            .slice()
-            .sort(
-              (a, b) =>
-                timestampToMilliseconds(a.timestamp) -
-                timestampToMilliseconds(b.timestamp)
-            )
-            .map((score, index) => (
-              <li key={index} className='truncate'>
-                {`${score.timestamp} - ${score.name} [${score.difficulty}]`}
-              </li>
-            ))}
-        </ul>
+        <main className='w-3/5 relative flex flex-col items-center'>
+          <div className='mt-6 drop-shadow-2xl'>
+            <Board board={board} onLeftClick={handleLeftClick} onRightClick={handleRightClick} />
+          </div>
 
-        <div className='mt-auto mr-2'>
-          {gameOver && <p className="azeret-mono text-red-400 font-semibold text-4xl tracking-wider w-full text-center">GAME OVER</p>}
-          {gameWon && <p className="azeret-mono text-green-400 font-semibold text-4xl tracking-wider w-full text-center">YOU WIN</p>}
-          <button
-            className={(gameOver || isPause ? 'bg-gray-400 cursor-not-allowed' : 'bg-amber-400 hover:bg-amber-500 cursor-pointer') + ` mt-2 px-4 py-2  rounded w-full`}
-            onClick={pause}
-          >
-            Pause
-          </button>
-          <button
-            className="mt-2 px-4 py-2 bg-amber-400 rounded  w-full hover:bg-amber-500 cursor-pointer"
-            onClick={resetGame}
-          >
-            {gameOver ? 'Retry' : 'Restart'}
-          </button>
-        </div>
-      </aside>
+          <div className='mt-auto w-full flex justify-baseline p-2'>
+            <p className='azeret-mono w-full'>Created by <span className='text-red-400 font-medium'>Pyromagne</span></p>
+            <div className='flex items-center'>
+              <input type="text"
+                readOnly={!editName}
+                className={`w-fit outline-none rounded ${editName ? 'bg-gray-200 text-left' : 'text-right cursor-default'}`}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                onKeyDown={(e) => {
+                  e.preventDefault();
+                  if (e.key === 'Enter') {
+                    setEditName(!editName)
+                  }
+                }}
+
+              />
+              <FaPen size={12} className='ml-2 cursor-pointer hover:text-amber-400' onClick={() => { setEditName(!editName) }} />
+            </div>
+          </div>
+
+        </main>
+
+        <aside className='w-1/5 pb-2 flex flex-col bg-gray-700/5'>
+          <p className='bg-amber-300 font-medium px-2'>Global Scoreboard</p>
+          <ul className="text-sm max-h-72 overflow-y-auto">
+
+          </ul>
+          <p className='bg-amber-300 font-medium px-2'>Local ScoreBoard</p>
+          <ul className="text-sm max-h-72 overflow-y-auto pl-2">
+            {scores
+              .slice()
+              .sort(
+                (a, b) =>
+                  timestampToMilliseconds(a.timestamp) -
+                  timestampToMilliseconds(b.timestamp)
+              )
+              .map((score, index) => (
+                <li key={index} className='truncate'>
+                  {`${score.timestamp} - [${score.difficulty.charAt(0)}] ${score.name}`}
+                </li>
+              ))}
+          </ul>
+        </aside>
+      </div>
     </div>
   );
 };
